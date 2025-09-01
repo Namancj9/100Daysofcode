4a0702c35c39fada54a3984e654a9e5d5ccfa952
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-09-01 16:50:43 +0530

    Question 5.c

---
+++ Day 1 to 10/Question 5.c
@@ -0,0 +1,23 @@
+/*
+Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+*/
+
+#include <stdio.h>
+int main(){
+    float f, c;
+    printf("Enter temperature in celcius:");
+    scanf("%f",&c);
+    f = (c * 9/5 + 32);
+    printf("Temperature in fahrenheit:%.2f",f);
+}
