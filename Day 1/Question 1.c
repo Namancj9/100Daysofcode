c5280ea815b74ad19ebe47ace35b2d09f04f50e4
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-09-17 20:24:54 +0530

    Create Question 1.c

---
+++ Day 1/Question 1.c
@@ -0,0 +1,13 @@
+/*Q1. Write a program to input two numbers and display their sum.*/
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    printf("Enter another number:");
+    scanf("%d",&b);
+    sum = a + b;
+    printf("Sum:%d", sum);
+    return 0;
+}
