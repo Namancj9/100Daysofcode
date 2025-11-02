c98b96f16a4a603274b0c6de33185e4b2821f405
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-11-02 21:53:56 +0530

    Implement sum calculation in Question 45

---
+++ Day 23/Question 45.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    float sum = 0.0;
+    int numerator = 2;
+    int denominator = 3;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum += (float)numerator / denominator;
+        numerator += 2;      
+        denominator += 4;    
+    }
+
+    printf("Approximate sum: %.2f\n", sum);
+    return 0;
+}
+
