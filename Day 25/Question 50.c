fde6b3911b2d2a284f921ce4624897aec4f446e0
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-11-02 22:01:34 +0530

    Create Question 50.c

---
+++ Day 25/Question 50.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+    int n = 5;
+
+    for(i = 0; i < n; i++) {
+        for(space = 0; space < i; space++) {
+            printf(" ");
+        }
+        for(j = i; j < n; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
