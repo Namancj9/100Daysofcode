666b927ab0de0506417d2a47bdc3d4e0abd07e76
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-11-02 23:22:30 +0530

    Create Question 97.c

---
+++ Day 49/Question 97.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[200];
+    int i;
+
+    printf("Enter your full name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = '\0';  
+
+    printf("Initials: ");
+
+    if (name[0] != ' ')
+        printf("%c", name[0]);
+
+    for (i = 1; name[i] != '\0'; i++) {
+        if (name[i - 1] == ' ' && name[i] != ' ')
+            printf("%c", name[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
