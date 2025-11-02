93a9e32c2d89725cc74b1c6f089d027b4327e5f7
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-11-02 23:59:58 +0530

    Create Question 113.c

---
+++ Day 63/Question 113.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i, j, temp;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    if (k > 0 && k <= n)
+        printf("The %dth smallest element is: %d\n", k, arr[k - 1]);
+    else
+        printf("Invalid input for k\n");
+
+    return 0;
+}
