46b9ac3158e4ea622236662af9d2c1f3c6d9e676
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-11-02 23:03:57 +0530

    Create Question 86.c

---
+++ Day 43/Question 86.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0, flag = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);  
+    
+    while (str[length] != '\0') {
+        if (str[length] == '\n') {
+            str[length] = '\0';
+            break;
+        }
+        length++;
+    }
+
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - i - 1]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+
+    return 0;
+}
