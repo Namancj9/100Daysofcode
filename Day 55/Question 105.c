c43e4954a048b654e191d8afd15d7fda0dba778d
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-11-02 23:42:57 +0530

    Create Question 105.c

---
+++ Day 55/Question 105.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+
+    int candidate = -1, count = 0;
+    for(int i = 0; i < n; i++) {
+        if(count == 0)
+            candidate = nums[i];
+        count += (nums[i] == candidate) ? 1 : -1;
+    }
+
+    count = 0;
+    for(int i = 0; i < n; i++) {
+        if(nums[i] == candidate)
+            count++;
+    }
+
+    if(count > n / 2)
+        printf("%d\n", candidate);
+    else
+        printf("-1\n");
+
+    return 0;
+}
