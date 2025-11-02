039c024377b624c6197fcbd7598a7351f582dc9f
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-11-02 22:47:12 +0530

    Create Question 81.c

---
+++ Day 41/Question 81.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);;  
+
+    for(i = 0; str[i] != '\0'; i++) {
+        count++;
+    }
+
+    printf("Number of characters in the string = %d\n", count);
+
+    return 0;
+}
