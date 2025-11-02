5dc8833575ca7b7db4c7434a9ffb4de331108b5d
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-11-03 00:17:41 +0530

    Create Question 120.c

---
+++ Day 70/Question 120.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+    char str[200];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    str[strcspn(str, "\n")] = '\0';
+
+    if (str[0] != '\0')
+        str[0] = toupper(str[0]);
+
+    for (i = 1; str[i] != '\0'; i++) {
+
+        if (str[i - 1] == ' ' && isalpha(str[i])) {
+            str[i] = toupper(str[i]);
+        } 
+        else {
+            str[i] = tolower(str[i]);
+        }
+    }
+
+    printf("Sentence Case: %s\n", str);
+
+    return 0;
+}
